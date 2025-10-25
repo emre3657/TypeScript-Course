d9b36f7e115b569f2ac972d1e8b91197a25a1a4d
Emre Ekinci <emree3657@gmail.com>
2025-10-25 11:42:02 +0300

    feat(types): learn nested types

--- basic.ts
+++ basic.ts
@@ -3,7 +3,7 @@ let myName: string = "Bob";
 let numberOfWheels: number = 4;
 let isStudent: boolean = false;
 
-// Custom types
+// Custom and Nested types
 type Food = string;
 let favoriteFood: Food = "pizza";
 
@@ -11,16 +11,33 @@ type Person = {
   name: string;
   age: number;
   isStudent: boolean;
+  address: Address;
+};
+
+type Address = {
+  street: string;
+  city: string;
+  country: string;
 };
 
 const person1: Person = {
   name: "Salih",
   age: 55,
   isStudent: false,
+  address: {
+    street: "Yazgülü",
+    city: "İstanbul",
+    country: "Türkiye",
+  },
 };
 
 const person2: Person = {
   name: "Emre",
   age: 20,
   isStudent: true,
+  address: {
+    street: "Yazgülü",
+    city: "İstanbul",
+    country: "Türkiye",
+  },
 };
