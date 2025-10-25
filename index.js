44918eaf4a00216de4f5fed49c52efa2ddecfcb6
Emre Ekinci <emree3657@gmail.com>
2025-10-25 08:06:08 +0300

    feat(app): start pizza console app

---
+++ index.js
@@ -0,0 +1,43 @@
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let nextOrderId = 0;
+let cashInRegister = 100;
+const orderQueue = [];
+
+function addNewPizza(pizzaObj) {
+  menu.push(pizzaObj);
+  return;
+}
+
+function placeOrder(name) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === name);
+  if (!selectedPizza) {
+    console.log("Invalid pizza name");
+    return;
+  }
+
+  cashInRegister += selectedPizza.price;
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+
+  return newOrder;
+}
+
+function completeOrder(orderId) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  if (!order) {
+    console.log(`No order with id: ${orderId}`);
+    return;
+  }
+  order.status = "completed";
+  return order;
+}
