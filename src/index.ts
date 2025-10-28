88cfa222c217188df06a90e2ebfbf0dac3d329ac
Emre Ekinci <emree3657@gmail.com>
2025-10-28 17:46:41 +0300

    feat(app): add id to Pizza and extend menu & order list

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 type Pizza = {
+  id: number;
   name: string;
   price: number;
 };
@@ -9,14 +10,14 @@ type Order = {
   status: "ordered" | "completed";
 };
 
-const menu = [
-  { name: "Margherita", price: 8 },
-  { name: "Pepperoni", price: 10 },
-  { name: "Hawaiian", price: 10 },
-  { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+  { id: 1, name: "Margherita", price: 8 },
+  { id: 2, name: "Pepperoni", price: 10 },
+  { id: 3, name: "Hawaiian", price: 10 },
+  { id: 4, name: "Veggie", price: 9 },
 ];
 
-let nextOrderId = 0;
+let nextOrderId = 1;
 let cashInRegister = 100;
 const orderQueue: Order[] = [];
 
@@ -53,3 +54,21 @@ function completeOrder(orderId: number) {
   order.status = "completed";
   return order;
 }
+
+// Add new pizzas
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 });
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 });
+
+// Place orders
+placeOrder("Chicken Bacon Ranch");
+placeOrder("Pepperoni");
+completeOrder(1);
+placeOrder("Anchovy");
+placeOrder("Viggie");
+completeOrder(2);
+
+// console log
+console.log("Menu", menu);
+console.log("Cash in register", cashInRegister);
+console.log("Order queue", orderQueue);
